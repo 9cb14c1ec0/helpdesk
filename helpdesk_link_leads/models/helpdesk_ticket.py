b6857c919d3373a7cf831dc08c2cbc132e067090
9cb14c1ec0 <9cb14c1ec0@sendmeemail.xyz>
2025-05-01 21:36:35 -0400

    more testing

--- helpdesk_link_leads/models/helpdesk_ticket.py
+++ helpdesk_link_leads/models/helpdesk_ticket.py
@@ -51,10 +51,5 @@ class HelpdeskTicket(models.Model):
             )
             if existing_tickets:
                 vals["related_ticket_ids"] = existing_tickets
-            res = super(HelpdeskTicket, self).create(vals)
-            for ticket in existing_tickets:
-                    ticket.write({"related_ticket_ids": [(4, vals["id"])]})
-        else:
-            res = super(HelpdeskTicket, self).create(vals)
-        return res
+        return super(HelpdeskTicket, self).create(vals)
     
